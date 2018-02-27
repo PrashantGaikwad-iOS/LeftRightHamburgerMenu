7f96560d252bb6d2ed7d385148571085b8abdf36
Prashant Gaikwad <prashant.gaikwad@leftrightmind.com>
2018-02-27 15:40:14 +0530

    Initial Commit

--- Day - 8 SideMenu/ViewController.swift
+++ Day - 8 SideMenu/ViewController.swift
@@ -10,14 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    @IBOutlet weak var alertButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sideMenus()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func sideMenus()  {
+        
+        if revealViewController() != nil {
+            
+            menuButton.target = revealViewController()
+            menuButton.action = #selector(revealViewController().revealToggle(_:))
+            
+            revealViewController().rightViewRevealWidth = 140
+            revealViewController().rearViewRevealWidth = 240
+            
+            
+            alertButton.target = revealViewController()
+            alertButton.action = #selector(revealViewController().rightRevealToggle(_:))
+            
+            self.view.addGestureRecognizer(revealViewController().panGestureRecognizer())
+            
+            
+            
+            
+        }
+        
     }
 
 
